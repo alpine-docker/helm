30c14260a8cf6b2379bffa6262cdd051a1f08239
Bill Wang <bill.wang>
2019-08-02 17:42:38 +1000

    feature/8 - deal with latest release tag

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ done
 # deal with latest release
 # <html><body>You are being <a href="https://github.com/helm/helm/releases/tag/v2.14.3">redirected</a>.</body></html>
 latest=$(curl -s https://github.com/${repo}/releases/latest)
-latest=$(echo $latest\" |grep -oP '(?<=tag\/)v[0-9][^"]*')
+latest=$(echo $latest\" |grep -oP '(?<=tag\/v)[0-9][^"]*')
 echo $latest
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then

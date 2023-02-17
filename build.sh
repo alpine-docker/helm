75e309e8c80dc1b12b5a0c589fd67fb34e463afe
Bill Wang <ozbillwang@users.noreply.github.com>
2023-02-17 16:57:38 +1100

    feature/multi-arch (#52)
    Co-authored-by: Bill Wang <bill.wang>

--- build.sh
+++ build.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
 # Prerequisite
-# Make sure you set secret enviroment variables in Travis CI
+# Make sure you set secret enviroment variables in CI
 # DOCKER_USERNAME
 # DOCKER_PASSWORD
-# API_TOKEN
 
 # set -ex
 
+set -e
+
+install_jq() {
+  # jq 1.6
+  DEBIAN_FRONTEND=noninteractive
+  #sudo apt-get update && sudo apt-get -q -y install jq
+  curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq
+  sudo mv jq /usr/bin/jq
+  sudo chmod +x /usr/bin/jq
+}
+
 build() {
 
-  echo "Found new version, building the image ${image}:${tag}"
-  docker build --no-cache --build-arg VERSION=${tag} -t ${image}:${tag} .
+  docker build --no-cache \
+    --build-arg HELM_VERSION=${tag} \
+    -t ${image}:${tag} .
 
   # run test
-  version=$(docker run -ti --rm ${image}:${tag} version --client)
-  #Client: &version.Version{SemVer:"v2.9.0-rc2", GitCommit:"08db2d0181f4ce394513c32ba1aee7ffc6bc3326", GitTreeState:"clean"}
-  if [[ "${version}" == *"Error: unknown flag: --client"* ]]; then
-    echo "Detected Helm3+"
-    version=$(docker run -ti --rm ${image}:${tag} version)
-    #version.BuildInfo{Version:"v3.0.0-beta.2", GitCommit:"26c7338408f8db593f93cd7c963ad56f67f662d4", GitTreeState:"clean", GoVersion:"go1.12.9"}
-  fi
+  echo "Detected Helm3+"
+  version=$(docker run --rm ${image}:${tag} helm version)
+  # version.BuildInfo{Version:"v3.6.3", GitCommit:"d506314abfb5d21419df8c7e7e68012379db2354", GitTreeState:"clean", GoVersion:"go1.16.5"}
+
   version=$(echo ${version}| awk -F \" '{print $2}')
   if [ "${version}" == "v${tag}" ]; then
     echo "matched"
@@ -29,48 +37,37 @@ build() {
     exit
   fi
 
-  if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  # install crane
+  curl -LO https://github.com/google/go-containerregistry/releases/download/v0.11.0/go-containerregistry_Linux_x86_64.tar.gz
+  tar zxvf go-containerregistry_Linux_x86_64.tar.gz
+  chmod +x crane
 
-    # multi-arch build
+  if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker buildx create --use
     docker buildx build --no-cache --push \
-		--build-arg VERSION=${tag} \
-		--platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/ppc64le,linux/s390x \
-		-t ${image}:${tag} .
+      --platform=linux/amd64,linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/ppc64le,linux/s390x \
+      --build-arg HELM_VERSION=${tag} \
+      -t ${image}:${tag} .
+
+  ./crane copy ${image}:${tag} ${image}:latest
+
   fi
 }
 
 image="alpine/helm"
-repo="helm/helm"
 
-if [[ ${CI} == 'true' ]]; then
-  latest=`curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${repo}/tags?per_page=100 |jq -r ".[].name"|sort -Vr|sed 's/^v//'`
-else
-  latest=`curl -sL https://api.github.com/repos/${repo}/tags?per_page=100 |jq -r ".[].name"|sort -Vr|sed 's/^v//'`
-fi
+install_jq
 
-for tag in ${latest}
-do
-  echo $tag
-  status=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/${tag})
-  echo $status
-  if [[ "${status}" =~ "not found" ]]; then
-    build
-  fi
-done
+# Get the list of all releases tags, excludes alpha, beta, rc tags
+tag=$(curl -s https://api.github.com/repos/helm/helm/releases | jq -r '.[].tag_name | select(test("alpha|beta|rc") | not) ' \
+    | sort -rV | head -n 1 |sed 's/v//')
 
-echo "Update latest image with latest release"
-# output format for reference:
-# <html><body>You are being <a href="https://github.com/helm/helm/releases/tag/v2.14.3">redirected</a>.</body></html>
-latest=$(curl -s https://github.com/${repo}/releases)
-latest=$(echo $latest\" |grep -oP '(?<=tag\/v)[0-9][^"]*'|grep -v \-|sort -Vr|head -1)
-echo $latest
+echo "Found helm latest version: ${tag}"
 
-# tag "latest" doesn't support multi-arch images, only for amd64
-if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
-  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-  docker pull ${image}:${latest}
-  docker tag ${image}:${latest} ${image}:latest
-  docker push ${image}:latest
+status=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/${tag})
+echo $status
+if [[ ( "${status}" =~ "not found" ) ||( ${REBUILD} == "true" ) ]]; then
+   echo "build image for ${tag}"
+   build
 fi
